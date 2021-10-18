802823c7657d9db891d42a566b3f56c03462a336
plagocastro <plagocastro@danielcastelao.org>
2021-10-18 09:52:43 +0200

    Initial commit

---
+++ app/src/main/java/com/dam2/mariasimonedice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dam2.mariasimonedice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
