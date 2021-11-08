d21e3fbd5fb1aaffca35fcd0babe38aa5aa69599
plagocastro <plagocastro@danielcastelao.org>
2021-11-08 11:13:21 +0100

    ACTUALIZACIONN 12 //JE

--- app/src/main/java/com/dam2/mariasimonedice/MainActivity.kt
+++ app/src/main/java/com/dam2/mariasimonedice/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     var contadorRonda: Int = 0
     var sec = arrayListOf<String>()
     var comprobacion = arrayListOf<String>()
+    var retraso = 1000L
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,36 +43,17 @@ class MainActivity : AppCompatActivity() {
 
         val botonInicio: Button = findViewById(R.id.inicio)
         botonInicio.setOnClickListener() {
-
             Toast.makeText(applicationContext, "COMIENZA", Toast.LENGTH_SHORT).show()
             botonInicio.setText("NUEVA RONDA")
             empezarJuego()
             botonInicio.setVisibility(GONE)
         }
-        val botonRojo: Button = findViewById(R.id.rojo)
-        botonRojo.setOnClickListener() {
-            Toast.makeText(applicationContext, "Rojo", Toast.LENGTH_SHORT).show()
-        }
-        val botonVerde: Button = findViewById(R.id.verde)
-        botonVerde.setOnClickListener() {
-            Toast.makeText(applicationContext, "Verde", Toast.LENGTH_SHORT).show()
-        }
-        val botonAzul: Button = findViewById(R.id.azul)
-        botonAzul.setOnClickListener() {
-            Toast.makeText(applicationContext, "Azul", Toast.LENGTH_SHORT).show()
-        }
-        val botonAmarillo: Button = findViewById(R.id.amarillo)
-        botonAmarillo.setOnClickListener() {
-            Toast.makeText(applicationContext, "Amarilolo", Toast.LENGTH_SHORT).show()
-        }
         val botonReinicio: Button = findViewById(R.id.reinicio)
         botonReinicio.setOnClickListener() {
-
             Toast.makeText(applicationContext, "REINICIADO", Toast.LENGTH_SHORT).show()
             reinicio()
         }
     }
-
     private fun empezarJuego() {
         mostrarRonda()
         val secuencia = GlobalScope.launch(Dispatchers.Main) {
@@ -83,12 +65,10 @@ class MainActivity : AppCompatActivity() {
         botonCom.setOnClickListener() {
             compruebo()
             botonCom.setVisibility(GONE)
-
         }
         sec.clear()
         comprobacion.clear()
     }
-
     private fun mostrarRonda() {
         contadorRonda = contadorRonda + 1
         val textoRonda: TextView = findViewById(R.id.texto_ronda)
@@ -96,55 +76,40 @@ class MainActivity : AppCompatActivity() {
         val botonCom: Button = findViewById(R.id.comprobar)
         botonCom.setVisibility(View.GONE)
     }
-
-
     suspend fun ejecutarSecuencia() {
         val botonAzul: Button = findViewById(R.id.azul)
         val botonRojo: Button = findViewById(R.id.rojo)
         val botonAmarillo: Button = findViewById(R.id.amarillo)
         val botonVerde: Button = findViewById(R.id.verde)
-        botonAzul.setClickable(FALSE)
-        botonVerde.setClickable(FALSE)
-        botonAmarillo.setClickable(FALSE)
-        botonRojo.setClickable(FALSE)
         for (i in 1..contadorRonda) {
             suspend fun azul1() {
-
                 botonAzul.setBackgroundColor(Color.parseColor("#18C4DA"))
-                delay(500L)
+                delay(retraso)
                 botonAzul.setBackgroundColor(Color.parseColor("#06525C"))
-                delay(500L)
+                delay(retraso)
                 sec.add("azul")
-
-
             }
             suspend fun rojo1() {
-
                 botonRojo.setBackgroundColor(Color.parseColor("#BA0B0B"))
-                delay(500L)
+                delay(retraso)
                 botonRojo.setBackgroundColor(Color.parseColor("#630404"))
-                delay(500L)
+                delay(retraso)
                 sec.add("rojo")
             }
-
             suspend fun amarillo1() {
-
                 botonAmarillo.setBackgroundColor(Color.parseColor("#FDE403"))
-                delay(500L)
+                delay(retraso)
                 botonAmarillo.setBackgroundColor(Color.parseColor("#A19206"))
-                delay(500L)
+                delay(retraso)
                 sec.add("amarillo")
             }
-
             suspend fun verde1() {
-
                 botonVerde.setBackgroundColor(Color.parseColor("#0CDC14"))
-                delay(500L)
+                delay(retraso)
                 botonVerde.setBackgroundColor(Color.parseColor("#046A07"))
-                delay(500L)
+                delay(retraso)
                 sec.add("verde")
             }
-
             val cadena = listOf(1, 2, 3, 4)
             val colores = cadena.shuffled().last()
             val dR = when (colores) {
@@ -152,50 +117,45 @@ class MainActivity : AppCompatActivity() {
                 2 -> verde1()
                 3 -> rojo1()
                 else -> azul1()
-
             }
-
         }
+        retraso = retraso - contadorRonda - 100
+        botonAzul.setEnabled(true)
+        botonVerde.setEnabled(true)
+        botonAmarillo.setEnabled(true)
+        botonRojo.setEnabled(true)
         delay(500L)
         Toast.makeText(applicationContext, "TU TURNO", Toast.LENGTH_SHORT).show()
         val botonCom: Button = findViewById(R.id.comprobar)
         botonCom.setVisibility(View.VISIBLE)
-        botonAzul.setClickable(TRUE)
-        botonVerde.setClickable(TRUE)
-        botonAmarillo.setClickable(TRUE)
-        botonRojo.setClickable(TRUE)
-
     }
-
     private fun tuTurno() {
 
             val botonRojo: Button = findViewById(R.id.rojo)
             botonRojo.setOnClickListener() {
-
                 comprobacion.add("rojo")
             }
             val botonVerde: Button = findViewById(R.id.verde)
             botonVerde.setOnClickListener() {
-
                 comprobacion.add("verde")
             }
             val botonAzul: Button = findViewById(R.id.azul)
             botonAzul.setOnClickListener() {
-
                 comprobacion.add("azul")
             }
             val botonAmarillo: Button = findViewById(R.id.amarillo)
             botonAmarillo.setOnClickListener() {
-
                 comprobacion.add("amarillo")
         }
+        botonAmarillo.setEnabled(false);
+        botonAzul.setEnabled(false);
+        botonVerde.setEnabled(false);
+        botonRojo.setEnabled(false);
     }
-
     fun compruebo() {
         val botonCom: Button = findViewById(R.id.comprobar)
-
         if (comprobacion == sec) {
-            Toast.makeText(applicationContext, "GANATE WACHOOOOOOOOO", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "PASATE DE RONDITA AHORA A LA SIGUIENTE ROLA :)", Toast.LENGTH_SHORT).show()
             if (contadorRonda==5){
                 val premios = GlobalScope.launch(Dispatchers.Main) {
                     premio()
@@ -206,7 +166,6 @@ class MainActivity : AppCompatActivity() {
                     premio2()
                     }
             }
-
             val botonInicio: Button = findViewById(R.id.inicio)
             botonInicio.setVisibility(VISIBLE)
         } else {
@@ -220,6 +179,8 @@ class MainActivity : AppCompatActivity() {
         println(comprobacion)
     }
     suspend fun premio() {
+        val botonInicio: Button = findViewById(R.id.inicio)
+        botonInicio.setVisibility(View.GONE)
         val botonCom: Button = findViewById(R.id.comprobar)
         botonCom.setVisibility(View.GONE)
         val botonRojo: Button = findViewById(R.id.rojo)
@@ -237,10 +198,12 @@ class MainActivity : AppCompatActivity() {
         botonAmarillo.setVisibility(View.VISIBLE)
         botonVerde.setVisibility(View.VISIBLE)
         botonRojo.setVisibility(View.VISIBLE)
-        botonCom.setVisibility(View.VISIBLE)
+        botonInicio.setVisibility(View.VISIBLE)
         premio.setVisibility(GONE)
     }
     suspend fun premio2() {
+        val botonInicio: Button = findViewById(R.id.inicio)
+        botonInicio.setVisibility(View.GONE)
         val botonCom: Button = findViewById(R.id.comprobar)
         botonCom.setVisibility(View.GONE)
         val botonRojo: Button = findViewById(R.id.rojo)
@@ -258,7 +221,7 @@ class MainActivity : AppCompatActivity() {
         botonAmarillo.setVisibility(View.VISIBLE)
         botonVerde.setVisibility(View.VISIBLE)
         botonRojo.setVisibility(View.VISIBLE)
-        botonCom.setVisibility(View.VISIBLE)
+        botonInicio.setVisibility(View.VISIBLE)
         premio.setVisibility(GONE)
     }
 
